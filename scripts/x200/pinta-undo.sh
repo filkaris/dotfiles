524802e5ffee020a3ac4a83482fb7da7cc2870c0
Filippos Karailanidis <phil.230@gmail.com>
2019-06-02 15:30:25 +0300

    Fix pinta ctrl-z

--- scripts/x200/pinta-undo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# depends:
-#  xdotool
-
-# Identify window by name (untitled 1* - Pinta)
-WINDOW=$(xdotool search --name "\- Pinta")
-
-# Execute undo
-xdotool key --window=$WINDOW ctrl+z
