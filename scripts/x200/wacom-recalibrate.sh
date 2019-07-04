d87cd6849c84909e0b0d586fe938447577ff3e30
Filippos Karailanidis <phil.230@gmail.com>
2019-07-04 20:42:34 +0300

    X200 touch scripts

---
+++ scripts/x200/wacom-recalibrate.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xinput --map-to-output 'Wacom Serial Penabled 1FG Touchscreen Pen stylus' LVDS-1
+xinput --map-to-output 'Wacom Serial Penabled 1FG Touchscreen Finger touch' LVDS-1
+xinput --map-to-output 'Wacom Serial Penabled 1FG Touchscreen Pen eraser' LVDS-1
