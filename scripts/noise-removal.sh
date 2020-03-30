75bd5b95e3dfa0ba47d692bf482fcefe59ef6994
Filippos Karailanidis <phil.230@gmail.com>
2020-03-30 20:26:03 +0300

    Add noise profile

--- scripts/noise-removal.sh
+++ scripts/noise-removal.sh
@@ -16,7 +16,7 @@ ffmpeg -y -i $1 -c:a flac $base.flac
 #ffmpeg -i input.mp4 -acodec pcm_s16le -ar 128k -vn -ss 00:00:00.0 -t 00:00:00.5 noiseaud.wav
 #sox noiseaud.wav -n noiseprof noise.prof
 
-sox $base.flac a-$base.flac noisered noise_c920 0.20
+sox $base.flac a-$base.flac noisered ~/.dotfiles/scripts/noise_c920 0.20
 
 ffmpeg -y -i "v-$base.$ext" -itsoffset 0.400 -i "a-$base.flac" -map 0:v -map 1:a -c:v libx264 -c:a aac $base-clean.$ext
 
